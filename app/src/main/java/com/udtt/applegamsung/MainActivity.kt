5b4493034abcdf4ee6db791b16ae69451ec74c31
Malibin <nightmare73@naver.com>
2020-03-14 20:22:55 +0900

    [init] git init, configure gradle

---
+++ app/src/main/java/com/udtt/applegamsung/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.udtt.applegamsung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.udtt.applegamsung.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
