5f39137841fc9262a1cf4e037a023cfd3f005ef1
Malibin <nightmare73@naver.com>
2020-03-27 01:40:24 +0900

    [add] 닉네임 입력 후 다음 버튼 누르면 키보드 내리게 만듦 하지만 내리면 존나게 느려짐. 매끄러운 느낌이 하나도 없음.. 대체 왜그런건지 모르게슴... 쓰레드파서 비동기로 돌려봐도 똑같았음..ㅠㅠ

--- app/src/main/java/com/udtt/applegamsung/ui/main/nickname/NicknameFragment.kt
+++ app/src/main/java/com/udtt/applegamsung/ui/main/nickname/NicknameFragment.kt
@@ -1,16 +1,19 @@
 package com.udtt.applegamsung.ui.main.nickname
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.udtt.applegamsung.databinding.FragmentNicknameBinding
 import com.udtt.applegamsung.ui.main.MainViewModel
 import com.udtt.applegamsung.ui.main.adapter.MainViewPagerAdapter.Companion.FRAGMENT_CATEGORIES
 import com.udtt.applegamsung.ui.util.BaseFragment
+import com.udtt.applegamsung.util.AsyncExecutor
 import org.koin.android.ext.android.inject
 
 class NicknameFragment : BaseFragment() {
@@ -43,10 +46,16 @@ class NicknameFragment : BaseFragment() {
         binding.btnNext.setOnClickListener {
             mainViewModel.movePageTo(FRAGMENT_CATEGORIES)
             nicknameViewModel.saveNickname()
+            hideSoftKeyBoard()
         }
         binding.btnBack.setOnClickListener { activity?.finish() }
 
         initAdmob(binding.banner)
+    }
+
+    private fun hideSoftKeyBoard() {
+        val imm = context?.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view?.rootView?.windowToken, 0)
     }
 
     companion object {
