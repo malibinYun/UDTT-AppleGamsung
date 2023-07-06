0d3f47f4aebb7a2b6398a47a5488862187457485
malibinYun <nightmare73@naver.com>
2023-07-06 23:44:57 +0900

    갬성 검사 결과의 등급을 저장할 Local Entity 새로 작성 및 기존 Model과 연결하는 Mapper 작성

---
+++ app/src/main/java/com/udtt/applegamsung/data/local/mapper/ApplePowerEntityMapper.kt
@@ -0,0 +1,24 @@
+package com.udtt.applegamsung.data.local.mapper
+
+import com.udtt.applegamsung.data.entity.ApplePower
+import com.udtt.applegamsung.data.entity.ApplePowerEntity
+
+fun ApplePowerEntity.toApplePower(): ApplePower {
+    return ApplePower(
+        name = name,
+        description = description,
+        minPower = minScore,
+        maxPower = maxScore,
+        id = id,
+    )
+}
+
+fun ApplePower.toApplePowerEntity(): ApplePowerEntity {
+    return ApplePowerEntity(
+        name = name,
+        description = description,
+        minScore = minPower,
+        maxScore = maxPower,
+        id = id,
+    )
+}
