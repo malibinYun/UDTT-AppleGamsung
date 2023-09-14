987089ff291c0b7341669e62f7666fd3bf48c80c
malibinYun <nightmare73@naver.com>
2023-09-14 22:30:31 +0900

    CategoriesRemoteDataSource coroutine 사용으로 변경

--- app/src/main/java/com/udtt/applegamsung/data/source/remote/CategoriesRemoteDataSource.kt
+++ app/src/main/java/com/udtt/applegamsung/data/source/remote/CategoriesRemoteDataSource.kt
@@ -2,16 +2,11 @@ package com.udtt.applegamsung.data.source.remote
 
 import com.google.firebase.firestore.FirebaseFirestore
 import com.udtt.applegamsung.data.entity.Category
+import com.udtt.applegamsung.data.remote.firestore.getDocumentSnapshots
 import com.udtt.applegamsung.data.source.CategoriesDataSource
-import com.udtt.applegamsung.data.remote.firestore.CATEGORIES_PATH
-import com.udtt.applegamsung.data.remote.firestore.getCollection
-import com.udtt.applegamsung.data.remote.firestore.toCategories
-import com.udtt.applegamsung.util.log
-import com.udtt.applegamsung.util.showStackTrace
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
 
 /**
  * Created By Yun Hyeok
@@ -19,20 +14,29 @@ import kotlinx.coroutines.tasks.await
  */
 
 class CategoriesRemoteDataSource(
-    private val fireStore: FirebaseFirestore
+    private val firestore: FirebaseFirestore
 ) : CategoriesDataSource {
 
     override fun getCategories(callback: (categories: List<Category>) -> Unit) {
-        CoroutineScope(Dispatchers.IO).launch {
-            fireStore.getCollection(CATEGORIES_PATH).await().toCategories()
+        CoroutineScope(Dispatchers.Main).launch {
+            val categories = firestore.collection(PathCategory)
+                .getDocumentSnapshots()
+                .map {
+                    Category(
+                        id = it.id,
+                        name = it.getString("name").orEmpty(),
+                        index = it.getLong("index")?.toInt() ?: 0,
+                    )
+                }
+            callback(categories)
         }
-
-        fireStore.getCollection(CATEGORIES_PATH)
-            .addOnSuccessListener { callback(it.toCategories()) }
-            .addOnFailureListener { log(it.showStackTrace()) }
     }
 
     override fun saveCategories(categories: List<Category>) {
-        // No Needed
+        throw UnsupportedOperationException("Do not call saveCategories() in remoteSource")
+    }
+
+    companion object {
+        private const val PathCategory = "categories"
     }
 }
