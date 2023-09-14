c4243f444fce47b0c65e5eab3fc3f65399809d4a
malibinYun <nightmare73@naver.com>
2023-09-14 23:02:54 +0900

    사용하지 않는 Firestore 확장 함수 제거

--- app/src/main/java/com/udtt/applegamsung/data/remote/firestore/FirestoreExt.kt
+++ app/src/main/java/com/udtt/applegamsung/data/remote/firestore/FirestoreExt.kt
@@ -1,14 +1,7 @@
 package com.udtt.applegamsung.data.remote.firestore
 
-import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.CollectionReference
-import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.DocumentSnapshot
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.QuerySnapshot
-import com.udtt.applegamsung.data.entity.ApplePower
-import com.udtt.applegamsung.data.entity.Category
-import com.udtt.applegamsung.data.entity.Product
 import kotlinx.coroutines.tasks.await
 
 /**
@@ -19,43 +12,3 @@ import kotlinx.coroutines.tasks.await
 suspend fun CollectionReference.getDocumentSnapshots(): List<DocumentSnapshot> {
     return this.get().await().documents
 }
-
-fun DocumentReference.getCollection(collectionPath: String): Task<QuerySnapshot> {
-    return this.collection(collectionPath).get()
-}
-
-fun FirebaseFirestore.getCollection(collectionPath: String): Task<QuerySnapshot> {
-    return this.collection(collectionPath).get()
-}
-
-fun FirebaseFirestore.getCategoryDocumentById(categoryId: String): DocumentReference {
-    return this.collection(CATEGORIES_PATH).document(categoryId)
-}
-
-fun QuerySnapshot.toCategories(): List<Category> {
-    return this.documents.map {
-        val category = it.toObject(Category::class.java) ?: throw CANNOT_CONVERT_EXCEPTION
-        category.apply { id = it.id }
-    }
-}
-
-fun QuerySnapshot.toProductsOf(categoryId: String): List<Product> {
-    return this.documents.map {
-        val product = it.toObject(Product::class.java) ?: throw CANNOT_CONVERT_EXCEPTION
-        product.apply { this.categoryId = categoryId }
-    }.sortedBy { it.name }
-}
-
-fun QuerySnapshot.toApplePowers(): List<ApplePower> {
-    return this.documents.map {
-        val applePower = it.toObject(ApplePower::class.java) ?: throw CANNOT_CONVERT_EXCEPTION
-        applePower.apply { this.id = it.id }
-    }.sortedBy { it.name }
-}
-
-const val CATEGORIES_PATH = "categories"
-const val PRODUCTS_PATH = "products"
-const val TEST_RESULTS_PATH = "testResults"
-const val APPLE_POWER_PATH = "ApplePower"
-
-private val CANNOT_CONVERT_EXCEPTION = IllegalStateException("객체를 변환할 수 없음")
