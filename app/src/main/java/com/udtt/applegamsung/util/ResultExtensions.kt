7517b39b223da729dc67e7770eed6e12d6eff8c3
malibinYun <nightmare73@naver.com>
2023-10-16 23:26:53 +0900

    Result<List<T>>.mapList(): Result<List<R>> 확장 함수 추가

--- app/src/main/java/com/udtt/applegamsung/util/ResultExtensions.kt
+++ app/src/main/java/com/udtt/applegamsung/util/ResultExtensions.kt
@@ -3,3 +3,7 @@ package com.udtt.applegamsung.util
 fun <T> Result<List<T>>.getOrEmpty(): List<T> {
     return this.getOrNull().orEmpty()
 }
+
+inline fun <T, R> Result<List<T>>.mapList(transform: (T) -> R): Result<List<R>> {
+    return this.map { it.map(transform) }
+}
